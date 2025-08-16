e1c0852b455172d5103b9ac05717c3d9a74ca7d4
AldiMusneldi <aldimusneldi@gmail.com>
2025-08-16 08:58:07 +0700

    feat(nav): add responsive navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { React, useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import NavBar from "./components/NavBar";
 function App() {
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <main className="bg-gray-950">
+      <NavBar />
+    </main>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,76 @@
+import { React, useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+
+export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const NavbarLinks = [
+    { id: 1, name: "Home", href: "#home" },
+    { id: 2, name: "About", href: "#about" },
+    { id: 3, name: "Skills", href: "#skills" },
+    { id: 4, name: "My Projects", href: "#projects" },
+  ];
+  return (
+    <header
+      className="fixed top-0 left-0 w-full z-20 text-black"
+      data-aos="fade-up"
+      data-aos-delay="300"
+    >
+      <div className="container mx-auto flex items-center justify-between p-5">
+        {/* Logo */}
+        <a href="#home " className="text-4xl font-bold italic text-black">
+          Portofolio
+        </a>
+        {/* Mobile Menu Button */}
+        <button
+          className="md:hidden focus:outline-none"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <FiMenu className="w-8 h-8 text-black" />
+        </button>
+        {/* Dekstop Navigation */}
+        <nav className="hidden md:flex items-center space-x-7">
+          {NavbarLinks.map((link) => (
+            <a
+              key={link.id}
+              href={link.href}
+              className="hover:text-gray-200 text-lg"
+            >
+              {link.name}
+            </a>
+          ))}
+          <button className="inline-flex text-black border-2 py-2 px-6 focus:outline-none hover:bg-purple-800 rounded-full text-lg">
+            {""}
+            Contact{""}
+          </button>
+        </nav>
+      </div>
+      {/* Mobile Navigation */}
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } md:hidden bg-[#801b9c] absolute top-0 left-0 w-full h-screen flex flex-col items-center justify-center space-y-8 pt-16`}
+      >
+        {/* Close Button */}
+        <button
+          className="absolute top-5 right-5 text-black"
+          onClick={() => setIsOpen(false)}
+        >
+          <FiX className="w-8 h-8" />
+        </button>
+        {/* Mobile Navigation */}
+        {NavbarLinks.map((link) => (
+          <a
+            key={link.id}
+            href={link.link}
+            className="text-lg text-black hover:text-gray-300"
+            onClick={() => setIsOpen(false)}
+          >
+            {link.name}
+          </a>
+        ))}
+        {/* Contact Button */}
+        <button className="inline-flex text-black border-2 py-2 px-6 focus:outline-none hover:bg-purple-800 rounded-full text-lg"></button>
+      </div>
+    </header>
+  );
+}

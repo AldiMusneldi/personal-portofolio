b6ee02081aa5fc53af05e44b7b95aa25f9a0bc8b
AldiMusneldi <aldimusneldi@gmail.com>
2025-08-17 18:09:51 +0700

    fix(nav): correct filename and remove skills link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,8 +6,7 @@ export default function NavBar() {
   const NavbarLinks = [
     { id: 1, name: "Home", href: "#home" },
     { id: 2, name: "About", href: "#about" },
-    { id: 3, name: "Skills", href: "#skills" },
-    { id: 4, name: "My Projects", href: "#projects" },
+    { id: 3, name: "My Projects", href: "#projects" },
   ];
   return (
     <header
